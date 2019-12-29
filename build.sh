f88bd35c93b59acd86f540db708e4ee2554bfbdb
ChristopherHX <44845461+ChristopherHX@users.noreply.github.com>
2019-12-29 16:51:45 +0100

    Github CI Patch

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PATH:$PWD/depot_tools
 git clone https://github.com/minecraft-linux/angle.git
 cd angle
-python scripts/bootstrap.py
-gclient sync
+/Users/runner/hostedtoolcache/Python/2.7.*/python scripts/bootstrap.py
+/Users/runner/hostedtoolcache/Python/2.7.*/python gclient sync
 find . -type f -exec sed -i "s/-Werror//g" {} \;
 gn gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_min="10.10" angle_enable_metal=false angle_enable_swiftshader=false angle_enable_vulkan=false'
 autoninja -C out/Release libEGL libGLESv2
