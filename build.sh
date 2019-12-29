cf526587aaa8bf93027b92ce6e5bbe8edc47c004
ChristopherHX <44845461+ChristopherHX@users.noreply.github.com>
2019-12-29 19:25:23 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PATH:$PWD/depot_tools
 git clone https://github.com/minecraft-linux/angle.git
-cd angle
+pushd angle
 python2 scripts/bootstrap.py
-python2 gclient.py sync
+python2 ../depot_tools/gclient.py sync
 find . -type f -print | xargs -n 1 sed -i '' 's/-Werror//g'
-python2 gn.py gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_min="10.12" angle_enable_metal=false angle_enable_swiftshader=false angle_enable_vulkan=false'
+python2 ../depot_tools/gn.py gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_min="10.12" angle_enable_metal=false angle_enable_swiftshader=false angle_enable_vulkan=false'
 autoninja -C out/Release libEGL libGLESv2
-cd ..
+popd
 mkdir ./artifacts
 cp angle/out/Release/*.dylib ./artifacts/ > /dev/null || :
