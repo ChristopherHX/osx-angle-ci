13130b072ff5fba8d9bb21658f43e7ebd390d31e
ChristopherHX <44845461+ChristopherHX@users.noreply.github.com>
2019-12-29 16:59:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ git clone https://github.com/minecraft-linux/angle.git
 cd angle
 /Users/runner/hostedtoolcache/Python/2.7.*/python scripts/bootstrap.py
 /Users/runner/hostedtoolcache/Python/2.7.*/python gclient sync
-find . -type f -exec sed -i "s/-Werror//g" {} \;
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/-Werror//g"
 gn gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_min="10.10" angle_enable_metal=false angle_enable_swiftshader=false angle_enable_vulkan=false'
 autoninja -C out/Release libEGL libGLESv2
 cd ..
