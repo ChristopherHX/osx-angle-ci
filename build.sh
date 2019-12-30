704e2ebbf1ebbb7a6464d5639b2d4a5fee791dd8
ChristopherHX <44845461+ChristopherHX@users.noreply.github.com>
2019-12-30 08:37:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git clone https://github.com/minecraft-linux/angle.git
 pushd angle
 python scripts/bootstrap.py
 gclient sync
-find build -name '*.gn' | xargs -n 1 sed -i '' 's/-Werror//g'
+find build -name '*.gn' | xargs -n 1 sed -i '' 's/-fatal_warnings//g'
 gn gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_path="'"${SDKROOT}"'" mac_sdk_min="10.12.0" mac_min_system_version="10.12.0" mac_deployment_target="10.12.0" angle_enable_metal=false angle_enable_swiftshader=false angle_enable_vulkan=false'
 autoninja -C out/Release libEGL libGLESv2
 popd
